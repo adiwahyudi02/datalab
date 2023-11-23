e52e33eeef2b5650d067a742d3a217b126df773f
adiwahyudi02 <adi.whyd02@gmail.com>
2023-11-23 22:31:03 +0700

    feat: add Search Input, User Card and User Detail Drawer component on Search page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,11 +1,26 @@
 'use client'
 
-import Header from '@/components/Header';
 import {
   Box,
+  useDisclosure,
 } from '@chakra-ui/react';
+import Header from '@/components/Header';
+import SearchInput from '@/components/SearchInput';
+import debounce from 'lodash.debounce';
+import UserCard from '@/components/user/UserCard';
+import UserDetailDrawer from '@/components/user/UserDetailDrawer';
 
-export default function Users() {
+export default function Search() {
+  const {
+    isOpen: isOpenDrawer,
+    onOpen: onOpenDrawer,
+    onClose: onCloseDrawer,
+  } = useDisclosure();
+
+  const handleSearch = debounce((val: string) => {
+    // handle logic here
+  }, 1000);
+
   return (
     <>
       <Header
@@ -14,9 +29,21 @@ export default function Users() {
       />
       <Box
         as="main"
-        maxW={96}
+        maxW="32rem"
         p={6}
       >
+        <SearchInput
+          placeholder="Search by email"
+          onSearch={handleSearch}
+          searchOnChange
+        />
+        <UserCard
+          onClickView={onOpenDrawer}
+        />
+        <UserDetailDrawer
+          isOpenDrawer={isOpenDrawer}
+          onCloseDrawer={onCloseDrawer}
+        />
       </Box>
     </>
   )
