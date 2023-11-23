06df189a0eae6079de3d118858c0f536f4f3607e
adiwahyudi02 <adi.whyd02@gmail.com>
2023-11-23 22:20:04 +0700

    feat: create SearchInput component

---
+++ components/SearchInput.tsx
@@ -0,0 +1,71 @@
+import { SearchIcon, SmallCloseIcon } from '@chakra-ui/icons';
+import {
+  Flex,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+} from '@chakra-ui/react';
+import { ChangeEvent, FormEvent, useState } from 'react';
+
+interface ISearchInputProps {
+  placeholder?: string;
+  searchOnChange?: boolean;
+  onSearch: (search: string) => void;
+};
+
+export default function SearchInput({
+  placeholder = 'Search',
+  searchOnChange = false,
+  onSearch,
+}: ISearchInputProps) {
+  const [search, setSearch] = useState('');
+
+  const handleSubmitSearch = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onSearch(search);
+  };
+
+  const handleChangeSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+    if (searchOnChange) onSearch(e.target.value);
+  }
+
+  const handleReset = () => {
+    setSearch('');
+    onSearch('');
+  };
+
+  return (
+    <form onSubmit={handleSubmitSearch}>
+      <Flex
+        direction="column"
+        gap="5"
+      >
+        <InputGroup>
+          <InputLeftElement pointerEvents="none">
+            <SearchIcon color="gray.400" />
+          </InputLeftElement>
+          <Input
+            type="text"
+            placeholder={placeholder}
+            value={search}
+            onChange={handleChangeSearch}
+          />
+          <InputRightElement>
+            <Flex
+              as="button"
+              type="button"
+              bg="gray.500"
+              color="white"
+              borderRadius="50%"
+              onClick={handleReset}
+            >
+              <SmallCloseIcon boxSize={3} />
+            </Flex>
+          </InputRightElement>
+        </InputGroup>
+      </Flex>
+    </form>
+  );
+}
