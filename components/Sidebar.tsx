7724a0f1172517a19a891eb71e96ab5adc0560a7
adiwahyudi02 <adi.whyd02@gmail.com>
2023-11-23 21:51:58 +0700

    feat: create Sidebar component

---
+++ components/Sidebar.tsx
@@ -0,0 +1,133 @@
+'use client'
+
+import {
+  Box,
+  Button,
+  Flex,
+  Icon,
+  Text,
+  Tooltip,
+} from '@chakra-ui/react';
+import NextLink from 'next/link';
+import { HiMenuAlt2, HiUsers } from 'react-icons/hi';
+import { MdSpaceDashboard } from 'react-icons/md';
+import { FaUserPlus, FaSearch } from 'react-icons/fa';
+import { usePathname } from 'next/navigation';
+import { useLocalStorage } from '@/hooks/useLocalStorage';
+
+const menu = [
+  {
+    href: '/',
+    title: 'Dashboard',
+    icon: MdSpaceDashboard,
+  },
+  {
+    href: '/users',
+    title: 'Users',
+    icon: HiUsers,
+  },
+  {
+    href: '/register',
+    title: 'Registration',
+    icon: FaUserPlus,
+  },
+  {
+    href: '/search',
+    title: 'Search',
+    icon: FaSearch,
+  },
+];
+
+export default function Sidebar() {
+  const pathName = usePathname();
+
+  const {
+    value: isOpen,
+    setValue: setIsOpen,
+  } = useLocalStorage('sidebar', false);
+
+  const onToggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <Box
+      as="aside"
+      w="15rem"
+      h="calc(100vh - 3.5rem)"
+      bg="ghostwhite"
+      boxShadow="gray 0.25rem 0 0.25rem -0.3rem"
+      overflowX="hidden"
+      transition="0.3s"
+      flexShrink={0}
+      zIndex={10}
+      {...(!isOpen) && {
+        w: '3.8rem'
+      }}
+    >
+      <Button
+        variant="unstyled"
+        display="block"
+        w="100%"
+        h="max-content"
+        borderRadius={0}
+        px={6}
+        py={5}
+        _hover={{
+          bg: 'blackAlpha.100',
+          transition: '0.3s',
+        }}
+        onClick={onToggleSidebar}
+      >
+        <Flex
+          align="center"
+          gap={4}
+        >
+          <Icon as={HiMenuAlt2} boxSize={5} />
+          <Text fontSize="sm" fontWeight="500">Menu</Text>
+        </Flex>
+      </Button>
+
+      {menu.map(item => (
+        <NextLink href={item.href} key={item.href}>
+          <Tooltip
+            hasArrow
+            label={item.title}
+            bg="gray.200"
+            color="black"
+            placement="right"
+            isDisabled={isOpen}
+          >
+            <Flex
+              align="center"
+              gap={4}
+              px={6}
+              py={5}
+              _hover={{
+                bg: 'blackAlpha.100',
+                transition: '0.3s',
+              }}
+              {...(item.href === pathName) && {
+                borderLeft: '0.25rem solid',
+                borderColor: 'blue.600',
+                color: 'blue.600',
+                bg: 'blackAlpha.100',
+              }}
+            >
+              <Icon
+                as={item.icon}
+                boxSize={5}
+              />
+              <Text
+                fontSize="sm"
+                fontWeight="500"
+              >
+                {item.title}
+              </Text>
+            </Flex>
+          </Tooltip>
+        </NextLink>
+      ))}
+    </Box >
+  );
+}
