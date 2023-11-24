3cc115c2c946cac8fe85f67d7b8a28c3c07ce132
adiwahyudi02 <adi.whyd02@gmail.com>
2023-11-24 21:22:00 +0700

    feat: add toast default configuration

--- providers/ChakraProvider.tsx
+++ providers/ChakraProvider.tsx
@@ -3,10 +3,19 @@
 import { CacheProvider } from '@chakra-ui/next-js';
 import { ChakraProvider } from '@chakra-ui/react';
 
+// toast default configuration
+const toastOptions = {
+  defaultOptions: {
+    variant: 'left-accent',
+    duration: 3000,
+    isClosable: true
+  }
+};
+
 export const Provider = ({ children }: { children: React.ReactNode }) => {
   return (
     <CacheProvider>
-      <ChakraProvider>
+      <ChakraProvider toastOptions={toastOptions}>
         {children}
       </ChakraProvider>
     </CacheProvider>
